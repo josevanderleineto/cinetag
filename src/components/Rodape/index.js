7c46693af04a2ce15271990b9e9ce69846546fec
Vanderlei Neto <jv.neto@outlook.com.br>
2024-07-09 22:56:27 -0300

    first commit

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Vanderlei Neto.</h2>
+        </footer>
+    )
+}
+
+export default Rodape;
